b5bfc4237935d96ff8c192c3e7da77b8b4df0727
Chinmayimullur4-art <chinmayimullur4@gmail.com>
2026-08-03 16:07:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ from langchain_groq import ChatGroq
 # -----------------------------
 # 🔐 ENV CONFIG
 # -----------------------------
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+GROQ_API_KEY = os.getenv("GROQ_BEC_API_KEY")
 
 if not GROQ_API_KEY:
     st.error("❌ GROQ_API_KEY not set in environment variables")
